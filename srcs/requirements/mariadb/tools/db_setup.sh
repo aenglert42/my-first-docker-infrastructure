731d66077d36ec320ef9655974f9f3c7caea7817
Andreas Englert <aenglert@2-I-1.42heilbronn.de>
2022-09-22 09:28:54 +0200

    cleaned up db_setup

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -2,12 +2,7 @@
 
 service mysql start
 
-#mariadb -u root -e "CREATE DATABASE ${DB_NAME};"
-#mariadb -u root -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_USER_PASSWORD}' WITH GRANT OPTION;"
-#mariadb -u root -e "ALTER USER 'root'@'localhost' identified by '${DB_ROOT_PASSWORD}';"
-
-mysql -u root -e "CREATE DATABASE ${DB_NAME};"
-mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -u root -e "CREATE DATABASE ${DB_NAME};"
+mariadb -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+mariadb -u root -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
